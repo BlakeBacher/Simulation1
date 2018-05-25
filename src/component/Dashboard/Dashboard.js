90e75f4efd314b724b5946e2d2915e7026d8627b
Blake Rindlisbacher <blakerindlisbacher@Blakes-MacBook-Pro.local>
2018-05-25 11:09:30 -0600

    Simulation1 First Commit

---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Product from '../Product/Product'
+
+class Dashboard extends Component{
+    render(){
+           
+        return(
+        <div>Dashboard
+             <div>
+               <Product/>
+            </div> 
+        </div>
+        )
+    }
+}
+
+export default Dashboard;
---
+++ src/component/Form/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class Form extends Component{
+    render(){
+            <div>Form</div> 
+        return(
+           <div>Form</div> 
+        )
+    }
+}
+
+export default Form;
